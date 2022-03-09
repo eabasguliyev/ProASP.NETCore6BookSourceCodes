201bc5d15d16244481dab99c44f50c6996c33697
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-10 01:24:31 +0400

    Checking null values

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+// using LanguageFeatures.Models;
+// using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers;
+
+public class HomeController: Controller{
+    public ViewResult Index(){
+        Product?[] products = Product.GetProducts();
+
+        Product? p = products[0];
+
+        string val;
+        if(p != null){
+            val = p.Name;
+        }else{
+            val = "No value";
+        }
+
+        return View(new string[] {
+            val
+        });
+    }
+}
