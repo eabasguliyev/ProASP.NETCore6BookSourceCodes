7b71fc1f0f9f1c150b84ab0301f2ad5d0a12b2d6
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-13 00:12:30 +0400

    Order controller and unit testing, displayin validation errors

--- SportsSln/SportsStore/Controllers/OrderController.cs
+++ SportsSln/SportsStore/Controllers/OrderController.cs
@@ -4,7 +4,35 @@ using SportsStore.Models;
 namespace SportsStore.Controllers;
 
 public class OrderController: Controller{
+    private readonly IOrderRepository repository;
+    private readonly Cart cart;
+
+    public OrderController(IOrderRepository repoService, Cart cartService)
+    {
+        this.repository = repoService;
+        this.cart = cartService;
+    }
+
     public ViewResult Checkout(){
         return View(new Order());
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Order order){
+        if(cart.Lines.Count() == 0){
+            ModelState.AddModelError("", "Sorry, your cart is empty!");
+        }
+
+        if(ModelState.IsValid){
+            order.Lines = cart.Lines.ToArray();
+            repository.SaveOrder(order);
+            cart.Clear();
+
+            return RedirectToPage("/Completed", new {
+                orderId = order.OrderId
+            });
+        }else{
+            return View();
+        }
     }
 }
--- SportsSln/SportsStore/Models/IOrderRepository.cs
+++ SportsSln/SportsStore/Models/IOrderRepository.cs
@@ -1,31 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace SportsStore.Models;
 
 public interface IOrderRepository{
     IQueryable<Order> Orders { get; }
     void SaveOrder(Order order);
-}
-
-public class EFOrderRepository: IOrderRepository{
-    private readonly StoreDbContext context;
-
-    public OrderRepository(StoreDbContext ctx)
-    {
-        this.context = ctx;
-    }
-
-    public IQueryable<Order> Orders => context.Orders
-                        .Include(o => o.Lines)
-                        .ThenInclude(l => l.Product);
-
-    public void SaveOrder(Order order){
-        context.AttachRange(order.Lines.Select(l => l.Product));
-
-        if(order.OrderId == 0){
-            context.Orders.Add(order);
-        }
-
-        context.SaveChanges();
-    }
 }
